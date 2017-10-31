b5d5de5bc7acf2f8816f2afa862b62e95a013100
TroelsHJ <jensen_troels@hotmail.com>
2017-10-31 10:33:23 +0100

    Added date interval search to ListTransation - format: ddmmyyy

--- budget-transaction/budget-transaction/Controllers/DefaultController.cs
+++ budget-transaction/budget-transaction/Controllers/DefaultController.cs
@@ -12,13 +12,41 @@ namespace budget_transaction.Controllers
 {
     public class DefaultController : ApiController
     {
+        [Route("api/Default/ListTransaction")]
         [HttpGet]
         public IHttpActionResult ListTransaction()
         {
             List<Transaction> transactions = Database.SelectAllTransactions(true);
+            Console.WriteLine("Bob");
+            return Ok(transactions);
+        }
+
+        [Route("api/Default/ListTransaction/{start}/{end}")]
+        [HttpGet]
+        public IHttpActionResult ListTransaction(string start, string end)
+        {
+            //input format of start and end string = ddmmyyyy
+
+            Console.WriteLine(start);
+            Console.WriteLine(end);
+            int startDate = Convert.ToInt32(start.Substring(0, 2));
+            int startMonth = Convert.ToInt32(start.Substring(2, 2));
+            int startYear = Convert.ToInt32(start.Substring(4, 4));
+            DateTime s = new DateTime(startYear, startMonth, startDate);
+            Console.WriteLine(s);
+
+            int endDate = Convert.ToInt32(end.Substring(0, 2));
+            int endMonth = Convert.ToInt32(end.Substring(2, 2));
+            int endYear = Convert.ToInt32(end.Substring(4, 4));
+            DateTime e = new DateTime(endYear, endMonth, endDate);
+            Console.WriteLine(e);
+
+
+            List<Transaction> transactions = Database.SelectAllTransactions(true, s, e);
 
             return Ok(transactions);
         }
+
 
         [Route("api/Default/GeneralInformation/{id}")]
         [HttpGet]
--- budget-transaction/budget-transaction/Database/DatabaseTransaction.cs
+++ budget-transaction/budget-transaction/Database/DatabaseTransaction.cs
@@ -95,19 +95,34 @@ namespace budget_transaction
             return transactions;
         }
 
-        public static void InsertTransaction(Transaction transaction)
+        public static List<Transaction> SelectAllTransactions(bool getName, DateTime StartDate, DateTime EndDate)
         {
             OpenConnection();
 
-            SqlCommand command = new SqlCommand("INSERT INTO [Transaction] (Value, Text, FK_Category, Active) VALUES (@Value, @Text, @FK_Category, @Active)", connection);
-            command.Parameters.Add(CreateParam("@Value", transaction.Value, SqlDbType.Decimal));
-            command.Parameters.Add(CreateParam("@Text", transaction.Text, SqlDbType.NVarChar));
-            command.Parameters.Add(CreateParam("@FK_Category", transaction.FK_Category, SqlDbType.Int));
-            command.Parameters.Add(CreateParam("@Active", transaction.Active, SqlDbType.Bit));
+            List<Transaction> transactions = new List<Transaction>();
+
+            SqlCommand command = new SqlCommand("SELECT * FROM [Transaction] WHERE Date BETWEEN @start and @end", connection);
+            command.Parameters.Add(CreateParam("@start", StartDate, SqlDbType.DateTime));
+            command.Parameters.Add(CreateParam("@end", EndDate, SqlDbType.DateTime));
 
             try
             {
-                command.ExecuteScalar();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Transaction transaction = new Transaction();
+
+                        transaction.Id = Int32.Parse(reader["Id"].ToString());
+                        transaction.Value = Decimal.Parse(reader["Value"].ToString());
+                        transaction.Text = reader["Text"].ToString();
+                        transaction.Date = DateTime.Parse(reader["Date"].ToString());
+                        transaction.Active = Convert.ToBoolean(reader["Active"].ToString());
+                        transaction.FK_Category = Int32.Parse(reader["FK_Category"].ToString());
+
+                        transactions.Add(transaction);
+                    }
+                }
             }
             catch (Exception ex)
             {
@@ -115,48 +130,18 @@ namespace budget_transaction
             }
 
             CloseConnection();
+
+            if (getName)
+            {
+                foreach (Transaction transaction in transactions)
+                {
+                    transaction.GetCategoryName();
+                }
+            }
+
+            return transactions;
         }
 
-        public static void UpdateTransaction(Transaction transaction)
-        {
-            OpenConnection();
 
-            SqlCommand command = new SqlCommand("UPDATE [Transaction] SET Value = @Value, Text = @Text, FK_Category = @FK_Category, Active = @Active WHERE Id = @Id", connection);
-            command.Parameters.Add(CreateParam("@Id", transaction.Id, SqlDbType.Int));
-            command.Parameters.Add(CreateParam("@Value", transaction.Value, SqlDbType.Int));
-            command.Parameters.Add(CreateParam("@Text", transaction.Text, SqlDbType.NVarChar));
-            command.Parameters.Add(CreateParam("@FK_Category", transaction.FK_Category, SqlDbType.Int));
-            command.Parameters.Add(CreateParam("@Active", transaction.Active, SqlDbType.Bit));
-
-            try
-            {
-                command.ExecuteNonQuery();
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-
-            CloseConnection();
-        }
-
-        public static void DeleteTransaction(int id)
-        {
-            OpenConnection();
-
-            SqlCommand command = new SqlCommand("DELETE FROM [Transaction] WHERE Id = @Id", connection);
-            command.Parameters.Add(CreateParam("@Id", id, SqlDbType.Int));
-
-            try
-            {
-                command.ExecuteNonQuery();
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-
-            CloseConnection();
-        }
     }
 }
