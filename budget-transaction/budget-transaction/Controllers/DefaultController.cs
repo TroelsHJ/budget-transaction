f7ddb3d03878ec6b1483b9defe7f97fa35bea04b
TroelsHJ <jensen_troels@hotmail.com>
2017-11-01 13:03:03 +0100

    corrected url params

--- budget-transaction/budget-transaction/Controllers/DefaultController.cs
+++ budget-transaction/budget-transaction/Controllers/DefaultController.cs
@@ -21,6 +21,7 @@ namespace budget_transaction.Controllers
             return Ok(transactions);
         }
 
+        /*
         [Route("api/Default/ListTransaction/{start}/{end}")]
         [HttpGet]
         public IHttpActionResult ListTransaction(string start, string end)
@@ -46,7 +47,32 @@ namespace budget_transaction.Controllers
 
             return Ok(transactions);
         }
+        */
+        [Route("api/Default/ListTransaction")]
+        [HttpGet]
+        public IHttpActionResult ListTransaction(string start, string end)
+        {
+            //input format of start and end string = ddmmyyyy
 
+            Console.WriteLine(start);
+            Console.WriteLine(end);
+            int startDate = Convert.ToInt32(start.Substring(0, 2));
+            int startMonth = Convert.ToInt32(start.Substring(2, 2));
+            int startYear = Convert.ToInt32(start.Substring(4, 4));
+            DateTime s = new DateTime(startYear, startMonth, startDate);
+            Console.WriteLine(s);
+
+            int endDate = Convert.ToInt32(end.Substring(0, 2));
+            int endMonth = Convert.ToInt32(end.Substring(2, 2));
+            int endYear = Convert.ToInt32(end.Substring(4, 4));
+            DateTime e = new DateTime(endYear, endMonth, endDate);
+            Console.WriteLine(e);
+
+
+            List<Transaction> transactions = Database.SelectAllTransactions(true, s, e);
+
+            return Ok(transactions);
+        }
 
         [Route("api/Default/GeneralInformation/{id}")]
         [HttpGet]
@@ -55,7 +81,7 @@ namespace budget_transaction.Controllers
             Transaction d = Database.SelectTransaction(id, false);
 
             string json = JsonConvert.SerializeObject(d);
-            
+
             return Ok(json);
 
         }
