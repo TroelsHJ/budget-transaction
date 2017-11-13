00ee963ac6fde6068dce52e1148b5b2803855e33
TroelsHJ <jensen_troels@hotmail.com>
2017-11-13 13:12:11 +0100

    added connection to MyWebSocket class

--- budget-transaction/budget-transaction/App_Start/WebApiConfig.cs
+++ budget-transaction/budget-transaction/App_Start/WebApiConfig.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 
+
 namespace budget_transaction
 {
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
-        {   
+        {
             // Web API configuration and services
+            MyWebSocket t = new MyWebSocket();
 
             // Web API routes
             config.MapHttpAttributeRoutes();
---
+++ budget-transaction/budget-transaction/MyWebSocket.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Web;
+
+namespace budget_transaction
+{
+    public class MyWebSocket
+    {
+        public MyWebSocket()
+        {
+            string url = string.Format("ws://{0}:{1}/socket.io/?EIO=3&transport=websocket", "localhost", 5000);
+            //var ws = new WebSocketSharp.WebSocket(url);
+
+            ClientWebSocket webSocketConnection = new ClientWebSocket();
+            Uri p = new Uri("ws://broker-service.herokuapp.com/socket.io/?EIO=3&transport=websocket");
+            webSocketConnection.ConnectAsync(p, CancellationToken.None);
+            Thread.Sleep(5000);
+        }
+    }
+}
