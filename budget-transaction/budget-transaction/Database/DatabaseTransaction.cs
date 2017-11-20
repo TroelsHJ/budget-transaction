8fe3f026142a1a33f52cc7aa85888d0abf43c901
Patrick Rechnitzer <you@example.com>
2017-11-20 14:35:01 +0100

    DatabaseTransaction add Insert, update and delete

--- budget-transaction/budget-transaction/Database/DatabaseTransaction.cs
+++ budget-transaction/budget-transaction/Database/DatabaseTransaction.cs
@@ -142,6 +142,69 @@ namespace budget_transaction
             return transactions;
         }
 
+        public static void InsertTransaction(Transaction transaction)
+        {
+            OpenConnection();
+
+            SqlCommand command = new SqlCommand("INSERT INTO [Transaction] (Value, Text, FK_Category, Active) VALUES (@Value, @Text, @FK_Category, @Active)", connection);
+            command.Parameters.Add(CreateParam("@Value", transaction.Value, SqlDbType.Decimal));
+            command.Parameters.Add(CreateParam("@Text", transaction.Text, SqlDbType.NVarChar));
+            command.Parameters.Add(CreateParam("@FK_Category", transaction.FK_Category, SqlDbType.Int));
+            command.Parameters.Add(CreateParam("@Active", transaction.Active, SqlDbType.Bit));
+
+            try
+            {
+                command.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            CloseConnection();
+        }
+
+        public static void UpdateTransaction(Transaction transaction)
+        {
+            OpenConnection();
+
+            SqlCommand command = new SqlCommand("UPDATE [Transaction] SET Value = @Value, Text = @Text, FK_Category = @FK_Category, Active = @Active WHERE Id = @Id", connection);
+            command.Parameters.Add(CreateParam("@Id", transaction.Id, SqlDbType.Int));
+            command.Parameters.Add(CreateParam("@Value", transaction.Value, SqlDbType.Int));
+            command.Parameters.Add(CreateParam("@Text", transaction.Text, SqlDbType.NVarChar));
+            command.Parameters.Add(CreateParam("@FK_Category", transaction.FK_Category, SqlDbType.Int));
+            command.Parameters.Add(CreateParam("@Active", transaction.Active, SqlDbType.Bit));
+
+            try
+            {
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            CloseConnection();
+        }
+
+        public static void DeleteTransaction(int id)
+        {
+            OpenConnection();
+
+            SqlCommand command = new SqlCommand("DELETE FROM [Transaction] WHERE Id = @Id", connection);
+            command.Parameters.Add(CreateParam("@Id", id, SqlDbType.Int));
+
+            try
+            {
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            CloseConnection();
+        }
 
     }
 }
